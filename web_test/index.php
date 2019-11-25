4cb9c36ba91fa876ebc3d31a88ef0915bb333c70
Patrick Madeng <patrickmaken@gmail.com>
2019-11-25 05:20:27 +0100

    web_test

---
+++ web_test/index.php
@@ -0,0 +1,23 @@
+<?php
+    $config = require 'configs.php'
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>FP MATCH SIMPLE WEB TEST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script> -->
+</head>
+<body>
+    <?= $config['fp_server_base_url'] ?>
+    <?= $config['auth_token'] ?>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
